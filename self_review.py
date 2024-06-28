1ced71dab9ca9266dbfe52754b69e7bcced2b1cd
Ajitesh Abhishek <ajiteshleo@gmail.com>
2024-06-28 14:23:09 -0700

    small fix

--- self_review.py
+++ self_review.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, ConfigDict
-from typing import List, Optional
+from typing import Dict, List, Optional
 from fastapi import UploadFile
 import re
 from llm import OpenAILLM, GoogleLLM, AnthropicLLM, GroqLLM
